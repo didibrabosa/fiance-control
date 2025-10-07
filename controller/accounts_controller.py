39bc3e184982ce2bafa99eaa91310ab52683dd3a
Diego Barbosa Vasconcelos <diegovasconcelosb@gmail.com>
2025-10-07 17:01:36 -0300

    feat: creating the controller layer and referencing in the main.py to seen the routes

---
+++ controller/accounts_controller.py
@@ -0,0 +1,18 @@
+import logging
+from fastapi import APIRouter
+from entities.accounts import Accounts
+from services.accounts_service import AccountsService
+from repositories.accounts_repository import AccountsRepository
+from configs.db_connection import get_db_connection
+
+logger = logging.getLogger(__name__)
+service = AccountsService(repository=AccountsRepository(db=get_db_connection()))
+router = APIRouter(tags=[Accounts])
+
+@router.post("/account")
+def create_accounts(account: Accounts):
+    logger.debug("Started to creating a account...")
+    account_created = service.create_accounts(account)
+
+    logger.debug(f"Created account = {account}")
+    return account_created
--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import logging
+from controller.accounts_controller import router
 
 from fastapi import FastAPI
 
 logger = logging.getLogger(__name__)
 
 app = FastAPI(title= "Finance API")
+app.include_router(router)
 
 @app.get("/health")
 async def health_check():
