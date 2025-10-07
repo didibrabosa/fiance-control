ddd281febe09360f7bdf5ccab9f88336e91904e2
Diego Barbosa Vasconcelos <diegovasconcelosb@gmail.com>
2025-10-07 17:07:38 -0300

    refactor: validating the entitie data with default values and more

--- entities/accounts.py
+++ entities/accounts.py
@@ -1,11 +1,12 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
+from typing import Optional
 
 class Accounts(BaseModel):
-    account_id: int
-    account_name: str
-    account_status: bool
-    balance: float
-    debt: float
-    created_at: datetime
-    updated_at: datetime
+    account_id: Optional[int] = Field(None, description="ID da conta, gerado automaticamente")
+    account_name: str = Field(..., description="Account name")
+    account_status: bool = Field(default=True, description="Account status")
+    balance: float = Field(default=0.00, description="Account balance")
+    debt: float = Field(default=0.00, description="Account debt")
+    created_at: datetime = Field(default_factory=datetime.now, description="Account create date")
+    updated_at: Optional[datetime] = Field(default=None, description="Account update date")
